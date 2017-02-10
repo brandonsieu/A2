23c9233afeb170ead37a969da8410a4354270528
Brandon Sieu <brandon.sieu@ucalgary.ca>
2017-02-10 11:44:17 -0700

    test

--- seng301-asgn2/seng301-asgn2/src/VendingMachineFactory.cs
+++ seng301-asgn2/seng301-asgn2/src/VendingMachineFactory.cs
@@ -5,18 +5,28 @@ using Frontend2.Hardware;
 
 public class VendingMachineFactory : IVendingMachineFactory {
 
+    List<VendingMachine> theVendingMachineFactory = new List<VendingMachine>();
+
     public int CreateVendingMachine(List<int> coinKinds, int selectionButtonCount, int coinRackCapacity, int popRackCapcity, int receptacleCapacity) {
+        theVendingMachineFactory.Add(new VendingMachine(coinKinds.ToArray(), selectionButtonCount, coinRackCapacity, popRackCapcity, receptacleCapacity));
         // TODO: Implement
         return 0;
     }
 
     public void ConfigureVendingMachine(int vmIndex, List<string> popNames, List<int> popCosts) {
+        if (vmIndex < 0 || vmIndex >= theVendingMachineFactory.Count) throw new Exception("Null vmIndex (config).");
+        theVendingMachineFactory[vmIndex].Configure(popNames, popCosts);
         // TODO: Implement
     }
 
     public void LoadCoins(int vmIndex, int coinKindIndex, List<Coin> coins) {
-        // TODO: Implement
-    }
+        if (vmIndex < 0 || vmIndex >= theVendingMachineFactory.Count) throw new Exception("Null vmIndex (LC).");
+        if (coinKindIndex < 0 || coinKindIndex >= theVendingMachineFactory[vmIndex].CoinRacks.Length) throw new Exception("Null coinkindIndex (LC).");
+        int[] coinstoLoad = new int[theVendingMachineFactory[vmIndex].CoinRacks.Length];
+       // cointstoLoad[coinKindIndex].addRange(coins);
+        //theVendingMachineFactory[vmIndex].LoadCoins(coins);
+    // TODO: Implement
+}
 
     public void LoadPops(int vmIndex, int popKindIndex, List<PopCan> pops) {
         // TODO: Implement
