94a7087825f25cd30801a31ff3cddfe5e655e6c8
Brandon Sieu <brandon.sieu@ucalgary.ca>
2017-02-11 18:31:56 -0700

    change

---
+++ seng301-asgn2/seng301-asgn2/src/VMData.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+    class VMData{
+
+    public int cashReceptacle = 0;
+    public List<int> coinKinds;
+
+    public VMData(List<int> coinKind){
+        coinKinds.AddRange(coinKind);    
+    }
+
+    public void addCash (int cash)
+    {
+        cashReceptacle += cash;
+    }
+
+    public void clearCash()
+    {
+        cashReceptacle = 0;
+    }
+    }
+
--- seng301-asgn2/seng301-asgn2/src/VendingMachineFactory.cs
+++ seng301-asgn2/seng301-asgn2/src/VendingMachineFactory.cs
@@ -6,11 +6,13 @@ using Frontend2.Hardware;
 public class VendingMachineFactory : IVendingMachineFactory {
 
     List<VendingMachine> theVendingMachineFactory = new List<VendingMachine>();
+    List<VMData> theVMLogics = new List<VMData>();
 
     public int CreateVendingMachine(List<int> coinKinds, int selectionButtonCount, int coinRackCapacity, int popRackCapcity, int receptacleCapacity) {
         theVendingMachineFactory.Add(new VendingMachine(coinKinds.ToArray(), selectionButtonCount, coinRackCapacity, popRackCapcity, receptacleCapacity));
+        theVMLogics.Add(new VMData(coinKinds));
         // TODO: Implement
-        return 0;
+        return theVendingMachineFactory.Count-1;
     }
 
     public void ConfigureVendingMachine(int vmIndex, List<string> popNames, List<int> popCosts) {
@@ -21,22 +23,52 @@ public class VendingMachineFactory : IVendingMachineFactory {
 
     public void LoadCoins(int vmIndex, int coinKindIndex, List<Coin> coins) {
         if (vmIndex < 0 || vmIndex >= theVendingMachineFactory.Count) throw new Exception("Null vmIndex (LC).");
-        if (coinKindIndex < 0 || coinKindIndex >= theVendingMachineFactory[vmIndex].CoinRacks.Length) throw new Exception("Null coinkindIndex (LC).");
-        int[] coinstoLoad = new int[theVendingMachineFactory[vmIndex].CoinRacks.Length];
-       // cointstoLoad[coinKindIndex].addRange(coins);
-        //theVendingMachineFactory[vmIndex].LoadCoins(coins);
+        VendingMachine theMachine = theVendingMachineFactory[vmIndex];
+        if (coinKindIndex < 0 || coinKindIndex >= theMachine.CoinRacks.Length) throw new Exception("Null coinkindIndex (LC).");
+        theMachine.CoinRacks[coinKindIndex].LoadCoins(coins);
     // TODO: Implement
 }
 
     public void LoadPops(int vmIndex, int popKindIndex, List<PopCan> pops) {
+        if (vmIndex < 0 || vmIndex >= theVendingMachineFactory.Count) throw new Exception("Null vmIndex (LP).");
+        VendingMachine theMachine = theVendingMachineFactory[vmIndex];
+        if (popKindIndex < 0 || popKindIndex >= theMachine.PopCanRacks.Length) throw new Exception("Null popkindIndex (LP).");
+        theMachine.PopCanRacks[popKindIndex].LoadPops(pops);
         // TODO: Implement
     }
 
     public void InsertCoin(int vmIndex, Coin coin) {
+        if (vmIndex < 0 || vmIndex >= theVendingMachineFactory.Count) throw new Exception("Null vmIndex (IC).");
+        VendingMachine theMachine = theVendingMachineFactory[vmIndex];
+        theMachine.CoinSlot.AddCoin(coin);
+        if (theVMLogics[vmIndex].coinKinds.Contains(coin.Value)) theVMLogics[vmIndex].addCash(coin.Value);
         // TODO: Implement
     }
 
     public void PressButton(int vmIndex, int value) {
+        if (vmIndex < 0 || vmIndex >= theVendingMachineFactory.Count) throw new Exception("Null vmIndex (PB).");
+        VendingMachine theMachine = theVendingMachineFactory[vmIndex];
+        if (value < 0 || theMachine.SelectionButtons.Length <= value) throw new Exception("Null value (PB).");
+        if (theMachine.PopCanCosts[value] <= theVMLogics[vmIndex].cashReceptacle){
+            theMachine.PopCanRacks[value].DispensePopCan();
+            theMachine.CoinReceptacle.StoreCoins();
+            int change = theVMLogics[vmIndex].cashReceptacle - theMachine.PopCanCosts[value];
+            List<int> sortedCoins = new List<int>();
+            sortedCoins.AddRange(theVMLogics[vmIndex].coinKinds);
+            sortedCoins.Sort();
+            while (change > 0)
+            {
+                int coinValue = sortedCoins[sortedCoins.Count - 1];
+                int modChange = change / coinValue;
+                CoinRack theCoinRack = theMachine.GetCoinRackForCoinKind(coinValue);
+                int amountRemoved = Math.Min(theCoinRack.Count, modChange);
+                change -= amountRemoved * coinValue;
+                for (int i = 0; i < amountRemoved; i++) theCoinRack.ReleaseCoin();
+                sortedCoins.RemoveAt(sortedCoins.Count - 1);
+                if (sortedCoins.Count == 0) break;
+            }
+            theVMLogics[vmIndex].cashReceptacle = 0;
+        }
         // TODO: Implement
     }
 
